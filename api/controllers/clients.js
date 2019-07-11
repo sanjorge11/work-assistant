d3dcfa19a1420be0ce7dff45e5ab34489dda1052
sanjorge11 <sanjorge@live.unc.edu>
2019-07-10 21:12:44 -0400

    working on Client schema

---
+++ api/controllers/clients.js
@@ -0,0 +1,42 @@
+
+var Client = require('../models/client');
+var mongoose = require('mongoose');
+
+exports.createClient = function(req, res, next) { 
+    
+    var client = new Client({
+        _id: new mongoose.Types.ObjectId(), 
+        firstName: req.body.firstName, 
+        lastName: req.body.lastName, 
+        fullName: (req.body.firstName + ' ' + req.body.lastName).trim(),
+        address: req.body.address, 
+        phoneNumber: req.body.phoneNumber, 
+        email: req.body.email
+    });
+
+    client.save().then(function(result) {
+        console.log(result); 
+
+        res.status(201).json({
+            message: 'Successfully added a new client',
+            client: {
+                _id: result._id, 
+                firstName: result.firstName, 
+                lastName: result.lastName, 
+                fullName: result.fullName,
+                address: result.address, 
+                phoneNumber: result.phoneNumber, 
+                email: result.email
+            }
+        });
+
+    }).catch(function(err) { 
+        console.log(err); 
+
+        res.status(500).json({
+            error: err
+        });
+
+    });
+
+}
---
+++ api/models /client.js
@@ -0,0 +1,19 @@
+
+var mongoose = require('mongoose');
+
+var clientSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId, 
+    firstName: { type: String, required: true }, 
+    lastName: { type: String, required: false },  
+    fullName: { type: String, required: false },  //will be just a concatenation of first and last name 
+    address: { type: String, required: true }, 
+    phoneNumber: { type: String, required: false }, 
+    email: {
+        type: String, 
+        required: true, 
+        unique: true, 
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ //regular expression for valid email address
+    }
+}); 
+
+module.exports = mongoose.model('Client', clientSchema);

4c032de31eb068237280b9a08fc5390f5ff52ee5
sanjorge11 <sanjorge@live.unc.edu>
2019-07-14 22:47:53 -0400

    Created AJAX request via Angular $http object to create a new client and to display list of clients.

--- static/controllers/clients-controller.js
+++ static/controllers/clients-controller.js
@@ -1,8 +1,42 @@
 
-app.controller("clients-controller", function ($scope) {
+app.controller("clients-controller", function ($scope, $http) {
     
     var vm = $scope; 
-    vm.currentState = 'clients';
-    console.log(vm.currentState); 
-    //$scope.msg = "I love London";
+    vm.formFirstName = '';
+    vm.formLastName = '';
+    vm.formAddress = '';
+    vm.formEmail = ''; 
+
+    
+    vm.getAllClients = function() {
+
+        $http.get("http://localhost:8080/clients")
+        .then(function(response) {
+            vm.clientsArr = response.data;
+        });
+
+    }
+
+    vm.createClient = function() {
+        
+        var url = "http://localhost:8080/clients"; 
+        var parameter = JSON.stringify(
+            {
+                firstName: vm.formFirstName, 
+                lastName: vm.formLastName, 
+                address: vm.formAddress, 
+                email: vm.formEmail
+            }
+        );
+
+        $http.post(url, parameter).
+        then(function(data) {
+            $('#myModal').modal("hide");    //hide modal and refresh with GET request 
+            vm.getAllClients(); 
+          });
+
+    }
+
+    vm.getAllClients(); 
+
 });

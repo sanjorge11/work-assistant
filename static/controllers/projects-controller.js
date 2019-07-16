a3396b96775e74081f0ca3593528199acc5c9174
sanjorge11 <sanjorge@live.unc.edu>
2019-07-15 21:14:29 -0400

    Added more to the front-end for Projects and Quotes. Needs more thought on UI design.

--- static/controllers/projects-controller.js
+++ static/controllers/projects-controller.js
@@ -1,5 +1,36 @@
 
-app.controller("projects-controller", function ($scope) {
-    console.log('project'); 
-    //$scope.msg = "I love London";
+app.controller("projects-controller", function ($scope, $http) {
+    
+    var vm = $scope; 
+    vm.formClientId = '';
+
+    
+    vm.getAllProjects = function() {
+
+        $http.get("http://localhost:8080/projects")
+        .then(function(response) {
+            vm.projectsArr = response.data;
+        });
+
+    }
+
+    vm.createProject = function() {
+        
+        var url = "http://localhost:8080/projects"; 
+        var parameter = JSON.stringify(
+            {
+                clientId: vm.formClientId
+            }
+        );
+
+        $http.post(url, parameter).
+        then(function(data) {
+            $('#myModal').modal("hide");    //hide modal and refresh with GET request 
+            vm.getAllProjects(); 
+          });
+
+    }
+
+    vm.getAllProjects(); 
+
 });
--- static/controllers/quotes-controller.js
+++ static/controllers/quotes-controller.js
@@ -1,5 +1,36 @@
 
-app.controller("quotes-controller", function ($scope) {
-    console.log('quote'); 
-    //$scope.msg = "I love London";
+app.controller("quotes-controller", function ($scope, $http) {
+    
+    var vm = $scope; 
+    vm.formClientId = '';
+
+    
+    vm.getAllQuotes = function() {
+
+        $http.get("http://localhost:8080/quotes")
+        .then(function(response) {
+            vm.quotesArr = response.data;
+        });
+
+    }
+
+    vm.createProject = function() {
+        
+        var url = "http://localhost:8080/quotes"; 
+        var parameter = JSON.stringify(
+            {
+                clientId: vm.formClientId
+            }
+        );
+
+        $http.post(url, parameter).
+        then(function(data) {
+            $('#myModal').modal("hide");    //hide modal and refresh with GET request 
+            vm.getAllQuotes(); 
+          });
+
+    }
+
+    vm.getAllQuotes(); 
+
 });
